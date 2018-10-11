f8d8c186f4a9c8768690df06951249cd69851656
twildasin <tjwild127@aol.com>
2018-10-11 15:58:58 -0400

    final commit

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,28 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+
+    private double mpg;
+    private double gallons;
+
+    public Automobile (double mileage)
+    {
+        mpg = mileage;
+        gallons = 0;
+    }
+
+    public void fillUp(double amount)
+    {
+        gallons += amount;
+    }
+
+    public void takeTrip(int distance)
+    {
+        gallons -= (distance / mpg);
+    }
+
+    public double reportFuel()
+    {
+        return gallons;
+    }
 }
